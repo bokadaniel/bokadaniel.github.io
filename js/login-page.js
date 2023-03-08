269ab20c729a8cec8bc5131ebc622283c42fe6ab
danielmdezfdez <danielmdezfdez@outlook.es>
2023-03-08 13:49:24 +0100

    Applied login styles.

--- js/login-page.js
+++ js/login-page.js
@@ -5,7 +5,6 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
-    const password = loginForm.password.value;
 
     if (username === "assignment1") {
         alert("You are succesfully login");
